0761ba7d812c2f8c7a6f0ba86fd80ffb688adda2
alanruttenberg <alanruttenberg@63f6da8f-821b-0410-a5cb-997393c68b30>
2010-05-28 20:23:06 +0000

    added begins/ceases relations

--- trunk/src/tools/bfo-relations.lisp
+++ trunk/src/tools/bfo-relations.lisp
@@ -1,22 +1,24 @@
 (defparameter *bfo2-relations*
-  '((part-of !obo:BFO_0000050)
+  '((is-part-of !obo:BFO_0000050)
     (has-part !obo:BFO_0000051)
     (inheres-in !obo:BFO_0000052)
-    (bearer-of !obo:BFO_0000053)
-    (realized-by !obo:BFO_0000054)
+    (is-bearer-of !obo:BFO_0000053)
+    (is-realized-by !obo:BFO_0000054)
     (realizes !obo:BFO_0000055)
     (participates-in !obo:BFO_0000056)
     (has-participant !obo:BFO_0000057)
-    (concretization-of !obo:BFO_0000058)
+    (is-concretization-of !obo:BFO_0000058)
     (concretizes !obo:BFO_0000059)
-    (immediately-preceded-by !obo:BFO_0000060)
+    (is-immediately-preceded-by !obo:BFO_0000060)
     (immediately-precedes !obo:BFO_0000061)
-    (preceded-by !obo:BFO_0000062)
+    (is-preceded-by !obo:BFO_0000062)
     (precedes !obo:BFO_0000063)
-    (course-of !obo:BFO_0000064)
+    (is-course-of !obo:BFO_0000064)
     (has-course !obo:BFO_0000065)
     (occurs-in !obo:BFO_0000066)
-    (contains-process !obo:BFO_0000067)
+    (has-site-of !obo:BFO_0000067)
+    (begins-to-exist-during !obo:BFO_0000068)
+    (ceases-to-exist-during !obo:BFO_0000069)
     ))
 
 (defun id-for-bfo-axiom (i)
@@ -36,18 +38,6 @@
        (progv (mapcar 'first ,classes) (mapcar 'second ,classes)
 	 ,@body))))
 
-(defmacro def-metadata (prop ont &rest prop-val)
-  `(setf (getf (get ',ont 'metadata) ',prop)
-	 (append
-	  (loop for (key val) on (getf (get ',ont 'metadata) ',prop) by #'cddr
-	     when (not (getf ',prop-val key))
-	     collect key collect val)
-	  ',prop-val)))
-
-(defmacro def-axiom (number ont axiom &optional doc)
-  `(setf (getf (get ',ont 'axioms) ',number)
-	 `(:number ,,number :documentation ,,doc :axioms ,',axiom)))
-
 (defun nax (id ax &optional label)
   "named axiom - id is a number. label is optional."
   `(,(first ax)  (annotation !obo:IAO_0010000 ,(id-for-bfo-axiom id))
@@ -61,16 +51,22 @@
 	(editor-preferred-label !obo:IAO_0000111)
 	(editor-note  !obo:IAO_0000116)
 	(axiom-id !obo:IAO_0010000))
-    (labels ((@en (s) (format nil "~a@en" (substitute #\space #\_ s)))
+    (labels ((@en (s) (format nil "~a@en" s))
 	     (label (thing s)  `(annotation-assertion !rdfs:label ,thing ,(@en s)))
+	     (alternative-label (thing s)  `(annotation-assertion ,alternative-term ,thing ,(@en s)))
 	     (definition (thing s)  `(annotation-assertion ,definition ,thing ,(@en s))))
-      (with-ontology rel (:about !obo:bfo/relations.owl :collecting t)
+      (with-ontology bfo-2-relations (:about !obo:bfo/core-relations.owl :collecting t)
 	  ((progv (mapcar 'car *bfo2-relations*) (mapcar 'second *bfo2-relations*)
 	     (with-label-vars-from "~/repos/bfo/trunk/src/ontology/bfo2.owl"  
-	       (asq (imports !obo:bfo.owl))
+	       (asq (imports !obo:bfo/core-classes.owl))
 	       (loop for (var uri) in *bfo2-relations* do
 		    (as `(declaration (object-property ,uri))
-			(label uri (substitute #\space #\- (string-downcase (string var))))))
+			(label uri (substitute #\space #\- (string-downcase (string var)))))
+		    (when (find #\- (string var))
+		      (as (alternative-label uri (substitute #\_ #\- (#"replaceFirst" (string-downcase (string var)) "is-" "")))))
+		    (if (#"matches" (string-downcase (string var)) "^is-.*")
+			(as (alternative-label uri (substitute #\space #\- (#"replaceFirst" (string-downcase (string var)) "is-" ""))))
+		    ))
 	       (as
 		`(declaration (annotation-property ,axiom-id))
 		`(annotation-assertion !rdfs:label ,axiom-id "axiom id")
@@ -80,52 +76,54 @@
 		`(declaration (annotation-property ,definition))
 		`(declaration (annotation-property ,editor-note)))
 	       (as
-		(nax 1 `(object-property-domain ,part-of ,entity) "domain of part of")
-		(nax 2 `(object-property-range ,part-of ,entity))
-		(nax 3 `(inverse-object-properties ,part-of ,has-part))
+		(nax 1 `(object-property-domain ,is-part-of ,entity) "domain of part of")
+		(nax 2 `(object-property-range ,is-part-of ,entity))
+		(nax 3 `(inverse-object-properties ,is-part-of ,has-part))
 		(nax 4 `(object-property-domain ,has-participant ,process))
 		(nax 5 `(object-property-range ,has-participant ,continuant))
 		(nax 6 `(inverse-object-properties ,has-participant ,participates-in))
-		(nax 7 `(object-property-domain ,bearer-of ,independent-continuant))
-		(nax 8 `(object-property-range ,bearer-of ,dependent-continuant))
-		(nax 9 `(inverse-object-properties ,inheres-in ,bearer-of))
+		(nax 7 `(object-property-domain ,is-bearer-of ,independent-continuant))
+		(nax 8 `(object-property-range ,is-bearer-of ,dependent-continuant))
+		(nax 9 `(inverse-object-properties ,inheres-in ,is-bearer-of))
 		(nax 10 `(object-property-domain ,has-course ,continuant))
 		(nax 11 `(object-property-range ,has-course ,process))
-		(nax 12 `(inverse-object-properties ,has-course ,course-of))
+		(nax 12 `(inverse-object-properties ,has-course ,is-course-of))
 		(nax 13 `(transitive-object-property ,has-part))
 		(nax 14 `(functional-object-property ,inheres-in))
 		(nax 15 `(object-property-domain ,immediately-precedes ,process))
 		(nax 16 `(object-property-range ,immediately-precedes ,process))
-		(nax 17 `(inverse-object-properties ,immediately-precedes ,immediately-preceded-by))
+		(nax 17 `(inverse-object-properties ,immediately-precedes ,is-immediately-preceded-by))
 
 		(nax 19 `(object-property-domain ,precedes ,process))
 		(nax 20 `(object-property-range ,precedes ,process))
-		(nax 21 `(inverse-object-properties ,precedes ,preceded-by))
-		(nax 22 `(sub-object-property-of ,immediately-preceded-by ,preceded-by))
+		(nax 21 `(inverse-object-properties ,precedes ,is-preceded-by))
+		(nax 22 `(sub-object-property-of ,is-immediately-preceded-by ,is-preceded-by))
 		(nax 23 `(sub-object-property-of ,immediately-precedes ,precedes))
 		(nax 24 `(object-property-domain ,occurs-in ,process))
 		(nax 25 `(object-property-range ,occurs-in ,independent-continuant))
-		(nax 26 `(inverse-object-properties ,occurs-in ,contains-process))
-		#|(nax 27 |# `(sub-object-property-of (object-property-chain ,part-of ,occurs-in) ,occurs-in); ) ;"part a process that occurs in C occurs in C")
-		#|(nax 18 |# `(sub-object-property-of (object-property-chain ,occurs-in ,part-of) ,occurs-in) ;) ;"if p occurs in c, then p occurs in anything that c is part of")
+		(nax 26 `(inverse-object-properties ,occurs-in ,has-site-of))
+		#|(nax 27 |# `(sub-object-property-of (object-property-chain ,is-part-of ,occurs-in) ,occurs-in); ) ;"part a process that occurs in C occurs in C")
+		#|(nax 18 |# `(sub-object-property-of (object-property-chain ,occurs-in ,is-part-of) ,occurs-in) ;) ;"if p occurs in c, then p occurs in anything that c is part of")
 
 		(nax 28 `(transitive-object-property ,precedes))
-		(nax 29 `(transitive-object-property ,preceded-by))
-		(nax 30 `(functional-object-property ,immediately-preceded-by))
+		(nax 29 `(transitive-object-property ,is-preceded-by))
+		(nax 30 `(functional-object-property ,is-immediately-preceded-by))
 		(nax 31 `(functional-object-property ,immediately-precedes))
 		(nax 32 `(object-property-domain ,concretizes ,generically-dependent-continuant))
 		(nax 33 `(object-property-range ,concretizes ,specifically-dependent-continuant))
-		(nax 34 `(inverse-object-properties ,concretizes ,concretization-of))
+		(nax 34 `(inverse-object-properties ,concretizes ,is-concretization-of))
 		(nax 35 `(object-property-domain ,realizes ,process))
 		(nax 36 `(object-property-range ,realizes ,realizable-entity))
-		(nax 37 `(inverse-object-properties ,realizes ,realized-by))
+		(nax 37 `(inverse-object-properties ,realizes ,is-realized-by))
 		#|(nax 38 |# `(sub-object-property-of (object-property-chain ,realizes ,inheres-in) ,has-participant) ; "bearers of realizables participate in their realizaton")  ; note OWLAPI bug.
+
+                (nax 39 `(inverse-object-properties ,begins-to-exist-during ,ceases-to-exist-during))
 		     ))))
-	(write-rdfxml rel "~/repos/bfo/trunk/src/ontology/bfo2-relations.owl")))))
+	(write-rdfxml bfo-2-relations "~/repos/bfo/trunk/src/ontology/bfo2-relations.owl")))))
 		
 ;(bfo2-relations)
 	     
-(def-metadata concretizes bfo
+(def-metadata concretizes bfo2-relations
   :editor-note
   "A generically dependent continuant may inhere in more than one entity. It does so by virtue of the fact that there is, for each entity that it inheres, a specifically dependent *concretization* of the generically dependent continuant that is specifically dependent. For instance, consider a story, which is an information artifact that inheres in some number of books. Each book bears some quality that carries the story. The relation between this quality and the generically dependent continuant is that the former is the concretization of the latter."
   :definition
@@ -137,7 +135,7 @@
 
 
 ;(def-axiom 1 bfo 
-;  `(object-property-domain ,part-of ,entity)
+;  `(object-property-domain ,is-part-of ,entity)
 ;  "domain of part of")
 
 
@@ -145,12 +143,12 @@
 ;(with-label-vars-from "~/repos/bfo/trunk/src/ontology/bfo2.owl"  entity) -> !obo:BFO_0000001
 
 ;; these axioms violate global constaints
-;		(nax 18 `(irreflexive-object-property ,immediately-preceded-by)) 
+;		(nax 18 `(irreflexive-object-property ,is-immediately-preceded-by)) 
 ;		(nax 19 `(irreflexive-object-property ,immediately-precedes))
-;		(nax 20 `(asymmetric-object-property ,immediately-preceded-by))
+;		(nax 20 `(asymmetric-object-property ,is-immediately-preceded-by))
 ;		(nax 21 `(asymmetric-object-property ,immediately-precedes))
 	   
-;		(nax 24 `(irreflexive-object-property ,preceded-by)) 
+;		(nax 24 `(irreflexive-object-property ,is-preceded-by)) 
 ;		(nax 25 `(irreflexive-object-property ,precedes))
-;		(nax 26 `(asymmetric-object-property ,preceded-by))
+;		(nax 26 `(asymmetric-object-property ,is-preceded-by))
 ;		(nax 27 `(asymmetric-object-property ,precedes))
