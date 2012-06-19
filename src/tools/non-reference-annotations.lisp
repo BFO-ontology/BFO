88211c7d79d5aa9fb263d5e96302ae34fcae8a5a
Alan Ruttenberg <alanruttenberg@gmail.com>
2012-06-19 18:03:08 +0000

    Start of a file of annotations that come from other than the BFO2 reference.

---
+++ src/tools/non-reference-annotations.lisp
@@ -0,0 +1,6 @@
+((annotation-assertion !editor-note 0d-cf-boundary  "zero dimension continuant fiat boundaries are not spatial points. Considering the example 'the quadripoint where the boundaries of Colorado, Utah, New Mexico, and Arizona meet' : There are many frames in which that point is zooming through many points in space. Whereas, no matter what the frame, the quadripoint is always in the same relation to the boundaries of Colorado, Utah, New Mexico, and Arizona.")
+ :source "Alan Ruttenberg"
+ :note "requested by Melanie Courtot"
+ :seeAlso !<https://groups.google.com/d/msg/bfo-owl-devel/s9Uug5QmAws/ZDRnpiIi_TUJ>
+ :id _)
+
