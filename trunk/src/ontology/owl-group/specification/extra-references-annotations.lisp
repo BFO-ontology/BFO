46939bf53e358b8efb0c186fb9f5f6a93ce6eac6
mcourtot <mcourtot@63f6da8f-821b-0410-a5cb-997393c68b30>
2012-06-29 21:40:05 +0000

    created file containing extra annotations coming from the reference but not part of the terms per se and therefore not included in the BFO to OWL automated translation

---
+++ trunk/src/ontology/owl-group/specification/extra-references-annotations.lisp
@@ -0,0 +1,8 @@
+((annotation-assertion !definition-source object-aggregate "Thomas Bittner and Barry Smith, 'A Theory of Granular Partitions', in K. Munn and B. Smith (eds.), Applied Ontology: An Introduction, Frankfurt/Lancaster: ontos, 2008, 125-158.")
+ :source "Melanie Courtot"
+ :id _)
+
+
+((annotation-assertion !editor-note object-aggregate "An entity a is an object aggregate if and only if there is a mutually exhaustive and pairwise disjoint partition of a into objects ")
+ :source "Melanie Courtot"
+ :id _)
