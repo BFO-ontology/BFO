25ca74b416081458b1bcf0c5f50dc9f419b61de1
Alan Ruttenberg <alanruttenberg@gmail.com>
2012-06-29 22:29:09 +0000

    mind your language ... en

--- src/ontology/owl-group/specification/non-reference-annotations.lisp
+++ src/ontology/owl-group/specification/non-reference-annotations.lisp
@@ -1,44 +1,44 @@
-((annotation-assertion !editor-note 0d-cf-boundary  "zero dimension continuant fiat boundaries are not spatial points. Considering the example 'the quadripoint where the boundaries of Colorado, Utah, New Mexico, and Arizona meet' : There are many frames in which that point is zooming through many points in space. Whereas, no matter what the frame, the quadripoint is always in the same relation to the boundaries of Colorado, Utah, New Mexico, and Arizona.")
+((annotation-assertion !editor-note 0d-cf-boundary  "zero dimension continuant fiat boundaries are not spatial points. Considering the example 'the quadripoint where the boundaries of Colorado, Utah, New Mexico, and Arizona meet' : There are many frames in which that point is zooming through many points in space. Whereas, no matter what the frame, the quadripoint is always in the same relation to the boundaries of Colorado, Utah, New Mexico, and Arizona.@en")
  :source "Alan Ruttenberg"
  :note "requested by Melanie Courtot"
  :seeAlso !<https://groups.google.com/d/msg/bfo-owl-devel/s9Uug5QmAws/ZDRnpiIi_TUJ>
  :id 1)
 
-((annotation-assertion !editor-note s-region  "Spatial region doesn't have a closure axiom because the subclasses don't exhaust all possibilites. An example would be the union of a spatial point and a spatial line that doesn't overlap the point, or two spatial lines that intersect at a single point. In both cases the resultant spatial region is neither 0-dimensional, 1-dimensional, 2-dimensional, or 3-dimensional.")
+((annotation-assertion !editor-note s-region  "Spatial region doesn't have a closure axiom because the subclasses don't exhaust all possibilites. An example would be the union of a spatial point and a spatial line that doesn't overlap the point, or two spatial lines that intersect at a single point. In both cases the resultant spatial region is neither 0-dimensional, 1-dimensional, 2-dimensional, or 3-dimensional.@en")
  :source "Alan Ruttenberg"
  :note "per discussion with Barry Smith"
  :id 2)
 
-((annotation-assertion !editor-note t-region  "Temporal region doesn't have a closure axiom because the subclasses don't exhaust all possibilites. An example would be the mereological sum of a temporal instant and a temporal interval that doesn't overlap the instant. In this case the resultant temporal region is neither 0-dimensional nor 1-dimensional")
+((annotation-assertion !editor-note t-region  "Temporal region doesn't have a closure axiom because the subclasses don't exhaust all possibilites. An example would be the mereological sum of a temporal instant and a temporal interval that doesn't overlap the instant. In this case the resultant temporal region is neither 0-dimensional nor 1-dimensional@en")
  :source "Alan Ruttenberg"
  :note "per discussion with Barry Smith"
  :id 3)
 
-((annotation-assertion !editor-note entity  "Entity doesn't have a closure axiom because the subclasses don't necessarily exhaust all possibilites. For example Werner Ceusters 'portions of reality' include 4 sorts, entities (as BFO construes them), universals, configurations, and relations. It is an open question as to whether entities as construed in BFO will at some point also include these other portions of reality. See, for example, 'How to track absolutely everything' at http://www.referent-tracking.com/_RTU/papers/CeustersICbookRevised.pdf")
+((annotation-assertion !editor-note entity  "Entity doesn't have a closure axiom because the subclasses don't necessarily exhaust all possibilites. For example Werner Ceusters 'portions of reality' include 4 sorts, entities (as BFO construes them), universals, configurations, and relations. It is an open question as to whether entities as construed in BFO will at some point also include these other portions of reality. See, for example, 'How to track absolutely everything' at http://www.referent-tracking.com/_RTU/papers/CeustersICbookRevised.pdf@en")
  :source "Alan Ruttenberg"
  :note "per discussion with Barry Smith"
  :seeAlso !<http://www.referent-tracking.com/_RTU/papers/CeustersICbookRevised.pdf>
  :id 4)
 
-((annotation-assertion !editor-note sdc  "Specifically dependent continuant doesn't have a closure axiom because the subclasses don't necessarily exhaust all possibilites. We're not sure what else will develop here, but for example there are questions such as what are promises, obligation, etc.")
+((annotation-assertion !editor-note sdc  "Specifically dependent continuant doesn't have a closure axiom because the subclasses don't necessarily exhaust all possibilites. We're not sure what else will develop here, but for example there are questions such as what are promises, obligation, etc.@en")
  :source "Alan Ruttenberg"
  :note "per discussion with Barry Smith"
  :id 5)
 
-((annotation-assertion !editor-note occurrent  "Occurrent doesn't have a closure axiom because the subclasses don't necessarily exhaust all possibilites. An example would be the sum of a process and the process boundary of another process.")
+((annotation-assertion !editor-note occurrent  "Occurrent doesn't have a closure axiom because the subclasses don't necessarily exhaust all possibilites. An example would be the sum of a process and the process boundary of another process.@en")
  :source "Alan Ruttenberg"
  :note "per discussion with Barry Smith"
  :id 6)
 
-((annotation-assertion !editor-note continuant "Continuant doesn't have a closure axiom because the subclasses don't necessarily exhaust all possibilites. For example, in an expansion involving bringing in some of Ceuster's other portions of reality, questions are raised as to whether universals are continuants")
+((annotation-assertion !editor-note continuant "Continuant doesn't have a closure axiom because the subclasses don't necessarily exhaust all possibilites. For example, in an expansion involving bringing in some of Ceuster's other portions of reality, questions are raised as to whether universals are continuants@en")
  :source "Alan Ruttenberg"
  :id 7)
 
-((annotation-assertion !editor-note cf-boundary "Continuant fiat boundary doesn't have a closure axiom because the subclasses don't necessarily exhaust all possibilites. An example would be the mereological sum of two-dimensional continuant fiat boundary and a one dimensional continuant fiat boundary that doesn't overlap it. The situation is analogous to temporal and spatial regions.")
+((annotation-assertion !editor-note cf-boundary "Continuant fiat boundary doesn't have a closure axiom because the subclasses don't necessarily exhaust all possibilites. An example would be the mereological sum of two-dimensional continuant fiat boundary and a one dimensional continuant fiat boundary that doesn't overlap it. The situation is analogous to temporal and spatial regions.@en")
  :source "Alan Ruttenberg"
  :id 8)
 
-((annotation-assertion !editor-note site "A site can move through spatial regions.  Consider the hold of a ship (site).  As it sails, the hold is moving through space, and changing its spatial region continuously.  Ditto for the captain's quarters, passenger rooms, etc. Spatial regions don't pass through one another.")
+((annotation-assertion !editor-note site "A site can move through spatial regions.  Consider the hold of a ship (site).  As it sails, the hold is moving through space, and changing its spatial region continuously.  Ditto for the captain's quarters, passenger rooms, etc. Spatial regions don't pass through one another.@en")
  :source "Reference"
  :id 9)
 
@@ -63,7 +63,7 @@ Here the material parts of a material entity are combined together causally by s
 
 CU3: Causal unity via engineered assembly of components
 
-Here the material parts of a material entity are combined together via mechanical assemblies joined for example through screws or other fasteners. The assemblies often involve parts which are reciprocally engineered to fit together, as in the case of dovetail joints, balls and bearings, nuts and bolts. A causal unity of this sort can be interrupted for a time, as when a watch is disassembled for repair, and then recreated in its original state. The parts of an automobile, including the moving parts, constitute an object because of their relative rigidity: while these parts may move with respect to each other, a given gear cannot move e.g., 10 ft., while the other parts do not.")
+Here the material parts of a material entity are combined together via mechanical assemblies joined for example through screws or other fasteners. The assemblies often involve parts which are reciprocally engineered to fit together, as in the case of dovetail joints, balls and bearings, nuts and bolts. A causal unity of this sort can be interrupted for a time, as when a watch is disassembled for repair, and then recreated in its original state. The parts of an automobile, including the moving parts, constitute an object because of their relative rigidity: while these parts may move with respect to each other, a given gear cannot move e.g., 10 ft., while the other parts do not.@en")
  :source "Reference"
  :id 10)
 
