0781c30f7ae09299780173c73420b1c799dd78a8
alanruttenberg@gmail.com <alanruttenberg@gmail.com@63f6da8f-821b-0410-a5cb-997393c68b30>
2012-11-27 19:13:37 +0000

    jie to update to fix bug

---
+++ releases/2012-11-15-bugfix/owl-group/specification/temporal-relation-axioms.lisp
@@ -0,0 +1,253 @@
+ ;; axioms starting 500
+
+ (object-property :temporal
+   (c-part-of c-has-part)
+   (temporal (:all :some) (:all :some) "as Mathias suggests")
+   (inverse-at-a-time :id 541)
+   (reflexive continuant :id 500)
+   (transitive-at-a-time :id 501)
+   (domain continuant :id 502)
+   (range continuant :id 503)
+   (material -> material :id 595)
+   (< (material -> (or material immaterial) :id 596))
+   (quality -> (not realizable) :id 577)
+   (realizable -> (not quality) :id 578)
+   (disposition -> (not role) :id 579)
+   (role -> (not disposition) :id 580)
+   (immaterial <- immaterial :id 581)
+   (< (0d-cf-boundary -> (not (or 1d-cf-boundary 2d-cf-boundary)) :id 591))
+   (< (1d-cf-boundary -> (not (or 2d-cf-boundary)) :id 582))
+   (< (0d-s-region -> (not (or 1d-s-region 2d-s-region 3d-s-region)) :id 583))
+   (< (1d-s-region -> (not (or 2d-s-region 3d-s-region)) :id 584))
+   (< (2d-s-region -> (not 3d-s-region) :id 592))
+   (s-region <-> s-region :id 585)
+   (< (cf-boundary -> cf-boundary :id 586))
+   (sdc -> (not (or ic gdc)) :id 587)
+   (gdc -> (not (or ic sdc)) :id 588)
+   (ic -> (not (or sdc gdc)) :id 589)
+   (< (immaterial -> (not material) :id 028-001))
+   (cf-boundary -> (not s-region) :id 029-001)
+   )
+
+;; This is for GO
+(object-property :temporal
+   (c-part-of-object c-has-part-object)
+   (temporal (:all) (:all))
+   (transitive-at-a-time :id 501)
+   (domain continuant :id 502)
+   (range continuant :id 503)
+   (material -> material :id )
+   (< (material -> (or material immaterial) :id 597))
+   (quality -> (not realizable) :id 577)
+   (realizable -> (not quality) :id 578)
+   (disposition -> (not role) :id 579)
+   (role -> (not disposition) :id 580)
+   (immaterial <- immaterial :id 581)
+   (< (0d-cf-boundary -> (not (or 1d-cf-boundary 2d-cf-boundary)) :id 591))
+   (< (1d-cf-boundary -> (not (or 2d-cf-boundary)) :id 582))
+   (< (0d-s-region -> (not (or 1d-s-region 2d-s-region 3d-s-region)) :id 583))
+   (< (1d-s-region -> (not (or 2d-s-region 3d-s-region)) :id 584))
+   (< (2d-s-region -> (not 3d-s-region) :id 592))
+   (s-region <-> s-region :id 585)
+   (< (cf-boundary -> cf-boundary :id 586))
+   (sdc -> (not (or ic gdc)) :id 587)
+   (gdc -> (not (or ic sdc)) :id 588)
+   (ic -> (not (or sdc gdc)) :id 589)
+   (< (immaterial -> (not material) :id 028-001))
+   (cf-boundary -> (not s-region) :id 029-001)
+   )
+
+((annotation-assertion !definition c-part-of-object_at
+		       "forall(t) exists_at(y,t) -> exists_at(x,t) and 'part of continuant'(x,y,t)")
+ :id 598)
+
+((annotation-assertion !editor-note c-part-of-object_at "This is a binary version of a ternary time-indexed, instance level, relation. Unlike the rest of the temporalized relations which temporally quantify over existence of the subject of the relation, this relation temporally quantifies over the existence of the object of the relation. The relation is provided tentatively, to assess whether the GO needs such a relation. It is inverse of 'has continuant part at all times'")
+ :id 599)
+
+((annotation-assertion !definition c-has-part-object_at
+		       "forall(t) exists_at(y,t) -> exists_at(x,t) and 'has continuant part'(x,y,t)")
+ :id 599)
+
+((annotation-assertion !editor-note c-has-part-object_at "This is a binary version of a ternary time-indexed, instance level, relation. Unlike the rest of the temporalized relations which temporally quantify over existence of the subject of the relation, this relation temporally quantifies over the existence of the object of the relation. The relation is provided tentatively, to assess whether the GO needs such a relation. It is inverse of 'part of continuant at all times'")
+ :id 600)
+
+((inverse-object-properties c-has-part-object_at c-part-of_at) :id 601)
+((inverse-object-properties c-part-of-object_at c-has-part_at) :id 602)
+
+ (object-property :temporal
+   (c-ppart-of c-has-ppart)
+   (issue 49)
+   (inverse-at-a-time :id 542)
+   (temporal (:all :some) (:all :some) "Mathias suggest not parallel to part of, seems not to alan")
+   (irreflexive :id 504)
+   (transitive-at-a-time :id 505)
+   (domain continuant :id 506)
+   (range continuant :id 507)
+   )
+
+ (object-property :temporal
+   (member-part-of has-member-part)
+   (issue 49)
+   (inverse-at-a-time :id 543)
+   (temporal (:all :some) (:all :some) "Seems to be similar enough to part of (parts aren't permanent or defining except at an instant, so offer full set")
+   (domain continuant :id 508)
+   (range continuant :id 509)
+   )
+
+ (object-property :temporal
+   (located-in has-location) 
+   (issue 49)
+   (inverse-at-a-time :id 544)
+   (temporal (:some :all) (:some :all))
+   (transitive-at-a-time :id 046-001)
+   (reflexive ic :id 511)
+   (domain ic :id 512)
+   (range ic :id 513)
+   )
+
+ (object-property :temporal
+   (located-at-r r-location-of) 
+   (issue 49)
+   (inverse-at-a-time :id 545)
+   (temporal (:some) (:some) "Include some some for now, but note that the all versions can be used to define frames")
+;   (reflexive s-region :id 042-001) not needed due to below self
+   (domain ic :id 041-002)
+   (range s-region :id 516)
+   (ic +> s-region :id 539) ; translates to located-at. We want there to be one at all times but this is the best we can do I think.
+   (s-region -> self :id 575)
+   (s (material -> 3d-s-region))
+   (< (s-region -> self :id 576))
+   (cf-boundary -> (or 0d-s-region 1d-s-region 2d-s-region) :id 029-001) ; check if two points can make a cf-boundary.
+   )
+
+ (object-property :temporal
+   (inheres-in bearer-of)
+   (issue 49)
+   (temporal (:all) (:some :all) "specific dependents inhere in their bearers for at all times they exist. So we defined all times for that direction and all and some times for the other. More broad than Mathias who suggests some times on the inverse too")
+   (inverse-at-a-time :id 547)
+   (domain sdc :id 517)
+   (sdc +> (and ic (not s-region))  :id 548)
+   (range (and ic (not s-region)) :id 518)
+   )
+
+  (object-property :temporal
+    (q-of has-q)
+    (inverse-at-a-time :id 549)
+    (temporal (:all) (:some :all) "specific dependents inhere in their bearers for at all times they exist. So we defined all times for that direction and all and some times for the other. More broad than Mathias who suggests some times on the inverse too")
+    (issue 49)
+    (domain-narrowed (inheres-in bearer-of) :id 564)
+    (domain quality :id 519)
+    )
+
+ (object-property :temporal
+   (f-of has-f)
+   (inverse-at-a-time :id 550)
+   (temporal (:all) (:some :all) "specific dependents inhere in their bearers for at all times they exist. So we defined all times for that direction and all and some times for the other. More broad than Mathias who suggests some times on the inverse too")
+   (issue 49)
+   (domain function :id 521)
+   (domain-narrowed (inheres-in bearer-of) :id 565))
+
+(object-property :temporal
+  (r-of has-r)
+  (inverse-at-a-time :id 551)
+  (temporal (:all) (:some :all) "specific dependents inhere in their bearers for at all times they exist. So we defined all times for that direction and all and some times for the other. More broad than Mathias who suggests some times on the inverse too")
+  (issue 49)
+  (domain-narrowed (inheres-in bearer-of) :id 566)
+  (domain role :id 523))
+
+ (object-property :temporal
+   (d-of has-d)
+   (inverse-at-a-time :id 552)
+   (temporal (:all) (:some :all) "specific dependents inhere in their bearers for at all times they exist. So we defined all times for that direction and all and some times for the other. More broad than Mathias who suggests some times on the inverse too")
+   (issue 49)
+   (domain disposition :id 525)
+   (domain-narrowed (inheres-in bearer-of) :id 567))
+
+ (object-property :temporal
+   (s-depends-on has-s-dep)
+   (inverse-at-a-time :id 553)
+   (transitive-at-at-time :id 594)
+   (temporal (:all :some) (:some :all) "specific dependents inhere in there bearers for their whole life. Buf the s-dependence of a process on a partcipant can hold only at some times. So both. I think Mathias leaves out the process case? (I would be happy to leave it out of BFO altogether")
+   (issue 49)
+   (domain (or process sdc) :id 527)
+   (range (or (and ic (not s-region)) sdc process) :id 528)
+   (s (process +> material))
+   (s (ic -> nothing :id 593))
+   )
+
+(object-property :temporal
+  (g-depends-on has-g-dep)
+  (inverse-at-a-time :id 554)
+  (temporal (:some) (:some) "Define some times in both directions agreeing with Mathias")
+  (issue 49)
+  (domain gdc :id 529)
+  (range (and ic (not s-region)) :id 530)
+  (< (s (o has-s-dep_st concretizes_at :id 573)))
+  (< (a (o has-s-dep_at concretizes_at :id 574)))
+  )
+
+(object-property :temporal
+  (has-material-basis material-basis-of)
+  (inverse-at-a-time :id 555)
+  (temporal (:all) (:some :all) "Follow Mathias as at all times for the forward relation, both for the reverse relation")
+  (issue 49)
+  (domain disposition :id 531)
+  (range material :id 532)
+  )
+
+(object-property :temporal
+   (concretizes concretized-by)
+   (inverse-at-a-time :id 556)
+   (temporal (:some :all) (:some :all))
+   (:issue 49   "Unsure of this one - Alan. Include both for now")
+   (domain sdc :id 533)
+   (range gdc :id 534)
+   (< (s (gdc +> sdc :id 557)))
+)
+ 
+(object-property :temporal
+  (st-projects-onto-s s-projection-of-st)
+  (inverse-at-a-time :id 558)
+  (temporal (:some) (:some) "Things tend to move and change shape in time, so at some times in both directions")
+  (issue 49)
+  (domain st-region :id 535)
+  (s (st-region +> s-region :id 590))
+  (range s-region :id 536)
+  )
+ 
+(object-property :temporal
+  (has-participant participates-in)
+  (temporal (:some :all) (:some :all) "at some times is parallels the class-class definition. At all times is permanent participation, requested by Stefan") (issue 49)
+  (inverse-at-a-time :id 559)
+  (domain process :id 537)
+  (range (and continuant (not s-region)) :id 538)
+  (a (o has-participant_at inheres-in_at :id 560)) ; if p has-participant sdc and sdc inheres-in m then p has-participant m 
+  (s (o has-participant_st inheres-in_at :id 561 :cant "There is a cyclic dependency involving property has-participant_st"))
+  (a (o bearer-of_at participates-in_at :id 562 :cant "There is a cyclic dependency involving property has-participant_at"))
+;  (< (s (o bearer-of_st participates-in_at :id 563)))  ; if m bearer-of sdc and sdc participates-in p then m participates-in p
+  (< (a (o has-g-dep_at participates-in_at :id 571 :cant "There is a cyclic dependency involving property has-participant_at")))
+;  (< (s (o has-g-dep_st participates-in_at :id 572)))  
+  )
+
+((sub-object-property-of (object-property-chain realizes inheres-in_at) has-participant_st)
+ :source "Alan Ruttenberg"
+ :id 106-002)
+
+((equivalent-classes r-quality (object-intersection-of quality (object-min-cardinality 2 inheres-in_at ic)))
+ :id 568)
+
+;(sub-object-property-of (object-property-chain located-at-r_st c-part-of_at r-location-of_at) located-in_st) 
+
+
+
+
+ 
+
+;; r1 r-location-of y y c-part-of x, x located-at r, y located-at r1 -> r1 c-part-of r 043-001
+ 
+
+
+
+
+
+
---
+++ releases/2012-11-15-bugfix/tools/bfo2-annotations.lisp
@@ -0,0 +1,314 @@
+(defparameter *bfo2-anntag-to-prop* 
+  '(("Synonym" "alternative-term")
+    ("Example" "example-of-usage")
+    ("Range" "editor-note")
+    ("Domain" "editor-note")
+    ("Definition" "definition")
+    ("EXAMPLES" "example-of-usage")
+    ("EXAMPLE" "example-of-usage")
+    ("Elucidation" "elucidation")
+    ("Axiom" "axiom-nl")
+    ("Theorem" "axiom-nl")))
+
+(defun generate-ontology-properties (bfo2)
+  (append (bfo-ontology-annotations bfo2) 
+	  (with-open-file (f "bfo:src;ontology;owl-group;specification;ontology-properties.lisp")
+	    (loop for entry = (read f nil :eof)
+	       until (eq entry :eof)
+	       if (consp (car entry)) collect (car entry) else collect entry))))
+
+(defun generate-ontology-annotation-property-defs (bfo2)
+  (let ((om (or (load-ontology "/Users/alanr/repos/information-artifact-ontology/trunk/src/ontology/ontology-metadata.owl")
+		(ignore-errors
+		  (load-ontology "https://information-artifact-ontology.googlecode.com/svn/trunk/src/ontology/ontology-metadata.owl")))
+		  ))
+    (list*
+     `(declaration (annotation-property ,!rdfs:isDefinedBy))
+     `(declaration (annotation-property ,!rdfs:seeAlso))
+     `(declaration (annotation-property ,!dc:contributor))
+     `(declaration (annotation-property ,!dc:member))
+     `(declaration (annotation-property ,!foaf:homepage))
+     `(declaration (annotation-property ,!bfo-owl-spec-label))
+     `(declaration (annotation-property ,!bfo-fol-spec-label))
+     (loop for (here-label prop) in (eval-uri-reader-macro *bfo2-ontprops*)
+	for label = (entity-label prop om)
+	collect `(declaration (annotation-property ,prop))
+	when label collect `(annotation-assertion !rdfs:label ,prop ,(concatenate 'string label "@en"))
+	when label collect `(annotation-assertion !rdfs:isDefinedBy ,prop ,!obo:iao.owl)))))
+
+(defun a-better-lousy-label (handle)
+  (concatenate
+   'string 
+   (or (a-handcrafted-label handle)
+       (loop with now = (string-downcase (string handle)) for (match replace) in
+	    '(("_at$" "-at-all-times")
+	      ("_st$" "-at-some-time")
+	      ("^c-" "continuant-")
+	      ("^o-" "occurrent-")
+	      ("^t-" "temporal-")
+	      ("^p-" "process-")
+	      ("-s-dep" "-specifically-dep")
+	      ("-s(-|$)" "-spatial-")
+	      ("-t-" "-temporal-")
+	      ("\\bcf\\b" "continuant-fiat")
+	      ("^s-d" "specifically-d")
+	      ("^g-" "generically-")
+	      ("^ic" "independent continuant")
+	      ("^sdc" "specifically-dependent-continuant")
+	      ("^gdc" "generically-dependent-continuant")
+	      ("^st-" "spatiotemporal-")
+	      ("\\bst\\b" "spatiotemporal")
+	      ("^s-" "spatial-")
+	      ("-dep-" "-dependent-")
+	      ("([0-3])d-" "$1.dimensional ") ;; I don't know why a "-" in the replacement works here. So change to "." and use another rule to replace with "-"
+	      ("-f-" "-function-")
+	      ("^r-q" "relational-q")
+	      ("\\br-l" "l")
+	      ("at-r" "at")
+	      ("profile-of" "process-profile-of")
+	      ("\\br\\b" "role")
+	      ("\\bq\\b" "quality")
+	      ("\\bf\\b" "function")
+	      ("\\bd\\b" "disposition")
+	      ("\\bg\\f" "generic")
+	      ("-t$" "-time")
+	      ("-" " ")
+	      ("^material$" "material entity")
+	      ("^immaterial$" "immaterial entity")
+	      ("\\." "-"))
+	    do
+	    (setq now (replace-all (#"replaceAll" now match replace) "^(\\d)-"
+				   (lambda(s) (format nil "~r-" (parse-integer s))) 1))
+	    finally (return now)))
+   "@en"))
+
+(defun a-handcrafted-label (handle)
+  (if (eq handle 'c-part-of-object_at)
+      "part of continuant at all times that whole exists" 
+      (if (eq handle 'c-has-part-object_at)
+	  "has continuant part at all times that part exists" 
+	  (let ((temporal (caar (all-matches (string handle) "_([AS])T$" 1))))
+	    (when temporal (setq handle (intern (#"replaceAll" (string handle) "_.T$" ""))))
+	    (print-db handle)
+	    (let ((matched 
+		   (getf '(st-projects-onto-s "projects onto spatial region"
+			   st-projects-onto-t "projects onto temporal region"
+			   s-projection-of_st "spatial projection of"
+			   t-projection-of_st "temporal projection of"
+			   s-depends-on "specifically depends on"
+			   has-s-dep "has specific dependent"
+			   g-depends-on "generically depends on"
+			   has-g-dep "has generic dependent"
+			   o-part-of "part of occurrent"
+			   c-part-of "part of continuant"
+			   t-part-of "temporal part of"
+			   member-part-of "member part of"
+			   o-ppart-of "proper part of occurrent"
+			   c-ppart-of "proper part of continuant"
+			   t-ppart-of "proper temporal part of"
+			   o-has-part "has occurrent part"
+			   c-has-part "has continuant part"
+			   has-t-part "has temporal part"
+			   has-member-part "has member part"
+			   o-has-ppart "has proper occurrent part"
+			   c-has-ppart "has proper continuant part"
+			   has-t-ppart "has proper temporal part"
+			   realizable "realizable entity"
+			   located-at-r "occupies spatial region"
+			   r-location-of "has spatial occupant"
+			   occupies "occupies spatiotemporal region"
+			   occupied-by "has spatiotemporal occupant"
+			   spans "occupies temporal region"
+			   span-of "has temporal occupant" 
+			   )
+			 handle)))
+	      (and matched
+		   (concatenate 
+		    'string  matched
+		    (if temporal
+			(if (equal temporal "A") " at all times" " at some time")
+			""))))))))
+
+(defun generate-label-annotations (bfo2)
+  (let ((seen (make-hash-table))
+	(axs nil))
+    (with-bfo-uris bfo2
+      (loop for (table type) in
+	   '((bfo-class2subclass class) (bfo-2prop2subprop object-property) (bfo-3prop2subprop Object-property))
+	   do
+	   (maphash (lambda(c sc) 
+		      (loop for el in (cons c sc)
+			 do
+			 (unless (gethash el seen)
+			   (push `(annotation-assertion !rdfs:label ,(eval el) ,(a-better-lousy-label el))
+				 axs)
+			   (push `(annotation-assertion !bfo-owl-spec-label ,(eval el) ,(string-downcase (string el)))
+				 axs)
+			   (let ((clifterm (or (second (assoc el (bfo-term2clif bfo2)))
+					       (second (assoc (intern (#"replaceAll" (string el) "_(S|A)T$" ""))
+							      (bfo-term2clif bfo2))
+						       ))))
+			     (when clifterm 
+			       (push `(annotation-assertion !bfo-fol-spec-label ,(eval el) ,clifterm)
+				 axs)))
+			   (setf (gethash el seen) t))))
+		    (funcall table bfo2)))
+      axs)))
+
+(defun parse-reference-annotations (bfo2)
+  (with-obo-metadata-uris
+    (loop with rawa = (extract-raw-bfo-annotations)
+       with table = (make-hash-table :test 'equalp)
+       for (annotation-type term text) in rawa
+       do
+	 (setq text (#"replaceAll" (#"replaceAll" text "^\\s+" "") "\\s+$" ""))
+	 (setq text (#"replaceAll" text "\\s*/\\*.*?\\*/\\s*" " ")) ; remove in line comments
+       ;; as|at|a|axiom|domain|range|note|example
+       (cond ((equalp annotation-type "note")
+	      (if (equalp term "ontology") 
+		  (push `(annotation !editor-note ,(format nil "BFO 2 Reference: ~a" text))
+			(bfo-ontology-annotations bfo2))
+		  (push (cons !editor-note (format nil "BFO 2 Reference: ~a" text))
+			(gethash (uri-for-reference-doc-term term bfo2) table))))
+	     ((#"matches" annotation-type "(?i)example.*")
+	      (loop for one in (split-at-regex text "\\s*\\\\[,;.]\\s*")
+		   do (push (cons !example-of-usage  one)
+			 (gethash (uri-for-reference-doc-term term bfo2) table))))
+	     ((member annotation-type '("as" "at" "a") :test 'equalp)
+	      (loop for (prop text) in (parse-as text)
+		 do 
+		   (push (cons prop text)
+		       (gethash (uri-for-reference-doc-term term bfo2) table))))
+	     (t nil))
+	 finally (progn (setf (bfo-term2annotation bfo2) table) (return table)))))
+
+(defun uri-for-reference-doc-term (tag bfo2)
+  (print-db tag)
+  (let ((found (or (assoc (intern (string-upcase tag)) (bfo-anntag2term bfo2) :test 'equalp)
+		   (assoc (intern (string-upcase (#"replaceAll" tag "[ _]" "-"))) (bfo-anntag2term bfo2) :test 'equalp))))
+    (unless found
+      (error "What's this tag: ~a ?" tag))
+    (let* ((found-or-translated
+	    (or (second found)
+		(intern (string-upcase (#"replaceAll" tag "[ _]" "-")))))
+	   (uri (third (assoc found-or-translated (cdr (bfo-uris bfo2)) :test 'equalp))))
+      (or found-or-translated
+	  (progn (warn "No URI found for ~a ?" found-or-translated) nil))
+      )))
+
+(defun parse-as (text)
+  (let ((them (all-matches text "\\b(\\w+):(.*)" 1 2)))
+    (when (> (length them) 1) (print them))
+    them))
+
+(defun generate-reference-annotations (bfo2 &aux axs)
+  (parse-reference-annotations bfo2)
+  (with-bfo-uris bfo2
+    (maphash (lambda (term as)
+	       (loop for (ann . rawtext) in as
+		  for (text axiomid) = (multiple-value-list (clean-reference-annotation-text rawtext ann term))
+		  for prop = (second (assoc ann *bfo2-anntag-to-prop* :test 'equalp))
+		  for uri = (or (and (uri-p ann) ann)
+				(second (assoc prop *bfo2-ontprops* :test 'equalp)))
+		  when (not (boundp term))
+		  do (setq axs (append (maybe-generate-annotation-for-temporal-property bfo2 term text axiomid prop uri)
+				       axs))
+		  unless(not (boundp term))
+		  do (setq axs (setq axs (append axs (generate-annotations-for-one-entry bfo2 prop axiomid uri (eval term) text))))))
+	       (bfo-term2annotation bfo2)))
+    axs)
+
+(defun clean-reference-annotation-text (text ann term)
+  (when (null text) (print-db ann term) (break))
+  (let ((clean (#"replaceAll" (string-trim " " text) "&amp;" "&")))
+    (let ((axiomid (caar (all-matches clean "\\[+(\\d+-\\d+)\\]*" 1))))
+      (if axiomid
+	  (progn
+	    (setq clean (#"replaceAll" clean "\\[+(\\d+-\\d+)\\]*" "(axiom label in BFO2 Reference: [$1])"))
+	    (values clean (make-uri nil (format nil "obo:bfo/axiom/~a" axiomid))))
+	  clean))))
+
+(defun generate-annotations-for-one-entry (bfo2 prop axiomid annotation-property-uri subject text &aux axs)
+  (when (and axiomid (gethash axiomid (bfo-fol-expressions bfo2)))
+    (push `(annotation-assertion ,@(and axiomid (list (list 'annotation !axiomid axiomid)))  ,!axiom-fol ,subject ,(gethash axiomid (bfo-fol-expressions bfo2))) axs))
+  (if  (equal prop "editor-note") 
+       (push `(annotation-assertion ,@(and axiomid (list (list 'annotation !axiomid axiomid)))  ,annotation-property-uri ,subject ,(format nil "BFO2 Reference: ~a" text)) axs)
+       (if (equal prop "example-of-usage")
+	   (loop for one in (split-at-regex text "\\s*\\\\[;,]\\s*")
+	      do (push `(annotation-assertion ,annotation-property-uri ,subject ,(concatenate 'string one "@en")) axs))
+	   (push `(annotation-assertion ,@(and axiomid (list (list 'annotation !axiomid axiomid)))
+					,annotation-property-uri ,subject ,(concatenate 'string text "@en")) axs)))
+  axs)
+
+
+(defun maybe-generate-annotation-for-temporal-property (bfo2 term text axiomid prop uri)
+  (let ((uris (cdr (bfo-uris bfo2)))
+	(axs nil))
+    (let* ((at (assoc (at-term term) uris))
+	   (st (assoc (st-term term) uris)))
+;      (print-db at st)
+      ;; lazy ass cut and paste. Factor out
+      (when st
+	(setq axs (append axs (generate-annotations-for-one-entry bfo2 prop axiomid uri (third st) text)))
+	(push `(annotation-assertion !editor-note ,(third st) ,(format nil "Alan Ruttenberg: This is a binary version of a ternary time-indexed, instance level, relation. The BFO reading of the binary relation '~a' is: exists t,  exists_at(x,t) & exists_at(y,t) & '~a'(x,y,t)" (a-better-lousy-label (car st)) (a-better-lousy-label term))) axs))
+      (when at 	
+	(setq axs (append axs (generate-annotations-for-one-entry bfo2 prop axiomid uri (third at) text)))
+	(push `(annotation-assertion !editor-note ,(third at) ,(format nil "Alan Ruttenberg: This is a binary version of a ternary time-indexed, instance-level, relation. The BFO reading of the binary relation '~a' is: forall(t) exists_at(x,t) -> exists_at(y,t) and '~a(x,y,t)'." (a-better-lousy-label (car at)) (a-better-lousy-label term))) axs))
+	axs)))
+
+      
+(defun gather-non-reference-annotations (bfo2)
+  (with-obo-metadata-uris
+    (with-bfo-uris bfo2
+      (with-open-file (f "bfo:src;ontology;owl-group;specification;non-reference-annotations.lisp")
+	(loop for entry = (eval-uri-reader-macro (read f nil :eof))
+	   until (eq entry :eof)
+	   append (generate-from-lispy-axiom bfo2 entry))))))
+
+(defun gather-extra-references-annotations (bfo2)
+  (with-obo-metadata-uris
+    (with-bfo-uris bfo2
+      (with-open-file (f "bfo:src;ontology;owl-group;specification;extra-references-annotations.lisp")
+	(loop for entry = (eval-uri-reader-macro (read f nil :eof))
+	   until (eq entry :eof)
+	   append (generate-from-lispy-axiom bfo2 entry))))))
+
+(defun eval-bfo-uris (form bfo2)
+  (with-bfo-uris bfo2
+      (cond ((and (symbolp form) (boundp form))
+	     (symbol-value form))
+	    ((atom form) form)
+	    (t (mapcar (lambda(el) (eval-bfo-uris el bfo2)) form)))))
+
+
+(defun read-bfo-specific-annotation-properties (bfo2)
+   (eval-uri-reader-macro
+    (with-open-file (f "bfo:src;ontology;owl-group;specification;bfo-specific-annotation-properties.lisp")
+      (with-bfo-uris bfo2
+	(with-obo-metadata-uris 
+	  (loop for form = (read f nil :eof)
+	     until (eq form :eof)
+	     collect form))))))
+
+			      
+(defun generate-inverse-annotations-duplicates (bfo2 partial-bfo)
+  (with-obo-metadata-uris
+    (let ((ont (if (v3kb-p partial-bfo) partial-bfo (load-ontology (namestring (truename partial-bfo))))))
+      (loop for (fromprop toprop annprop value) in
+	   (sparql '(:select (?p ?pi ?a ?v) (:distinct t) 
+		     (?a !rdf:type !owl:AnnotationProperty)
+		     (:union
+		      ((?p !rdf:type !owl:ObjectProperty) 
+		       (?pi !owl:inverseOf ?p))
+		      ((?p !rdf:type !owl:ObjectProperty) 
+		       (?p !owl:inverseOf ?pi)))
+		     (?p ?a ?v)
+		     (:filter (or (equal ?a !editor-note) (equal ?a !elucidation) (equal ?a !example-of-usage) (equal ?a !definition))
+		      ))
+		   :kb ont :use-reasoner :none :use-reasoner :pellet)
+	   ;do (print-db fromprop toprop annprop value (format nil "(from inverse property - ~a) ~a" (car (rdfs-label fromprop ont)) value))
+	   collect
+	   `(annotation-assertion ,annprop ,toprop ,(format nil "[copied from inverse property '~a'] ~a"
+							    (car (rdfs-label fromprop ont)) value))
+	   ))))
+							
