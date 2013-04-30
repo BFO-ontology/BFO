f331ab8a74e7b197f8787ac9616cfe74bcea4808
alanruttenberg@gmail.com <alanruttenberg@gmail.com@63f6da8f-821b-0410-a5cb-997393c68b30>
2013-04-30 22:35:08 +0000

    started for issue 144

---
+++ trunk/src/ontology/owl-group/specification/gcis-with-no-other-home.lisp
@@ -0,0 +1,11 @@
+;; axioms starting at 800
+
+((sub-object-property-of (object-inverse-of !has-bearer-at-some-time) !inheres-in-at-all-times)
+ :source "Alan Ruttenberg"
+ :issue 144
+ :id 700)
+
+((sub-object-property-of (object-inverse-of !has-bearer-at-all-times) !inheres-in-at-all-times)
+ :source "Alan Ruttenberg"
+ :issue 144
+ :id 701)
